580b1238267b1b517f16154831c9b89a57d35b29
Arasakki <pacificsalvador999@gmail.com>
2024-03-13 16:33:06 +0500

    1

---
+++ client/src/components/AddStudent/index.tsx
@@ -0,0 +1,163 @@
+import s from './index.module.scss'
+import * as mui from '@mui/material'
+import Line from '../Line'
+import Search from '../../assets/search'
+import {useState} from 'react'
+import Arrow, {ArrowType} from '../../assets/arrow'
+import microSVG from '../../assets/Microphone1.svg'
+import Listen from '../../assets/Listen.svg'
+import Plus from '../../assets/ItemPlus.svg'
+interface IAddStudent {}
+
+const AddStudent = ({}: IAddStudent) => {
+	// Block Student
+	const [nameStudent, setNameStudent] = useState<string>('')
+	const [contactFace, setContactFace] = useState<string>('')
+	const [phoneNumber, setPhoneNumber] = useState<string>('')
+	const [email, setEmail] = useState<string>('')
+	const [address, setAddress] = useState<string>('')
+	const [linkStudent, setLinkStudent] = useState<string>('')
+	const [costStudent, setCostStudent] = useState<string>('')
+	const [commentStudent, setCommentStudent] = useState<string>('')
+
+	// Block item
+
+	return (
+		<div className={s.wrapper}>
+			<div className={s.wrapperMenu}>
+				<div className={s.HeaderAddStudent}>
+					<div className={s.dataSlidePicker}>
+						<button className={s.btn}>
+							<span>
+								<Arrow direction={ArrowType.left} />
+							</span>
+						</button>
+						<p className={s.btnText}>Карточка ученика &frac14;</p>
+						<button className={s.btn}>
+							<span>
+								<Arrow direction={ArrowType.right} />
+							</span>
+						</button>
+					</div>
+				</div>
+				<div className={s.StudentInput}>
+					<div className={s.StudentCard}>
+						<p>Имя:</p>
+						<input
+							type="text"
+							value={nameStudent}
+							onChange={(e) => setNameStudent(e.target.value)}
+						/>
+					</div>
+
+					<Line width="296px" className={s.Line} />
+
+					<div className={s.StudentCard}>
+						<p>Контактное лицо:</p>
+						<input
+							type="text"
+							value={contactFace}
+							onChange={(e) => setContactFace(e.target.value)}
+						/>
+					</div>
+
+					<Line width="296px" className={s.Line} />
+
+					<div className={s.StudentCard}>
+						<p>Тел:</p>
+						<input
+							type="text"
+							value={phoneNumber}
+							onChange={(e) => setPhoneNumber(e.target.value)}
+							placeholder="+7 (___) ___-__"
+						/>
+						<div className={s.PhoneIcons}></div>
+					</div>
+					<Line width="296px" className={s.Line} />
+					<div className={s.StudentCard}>
+						<p>Эл. почта:</p>
+						<input
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<Line width="296px" className={s.Line} />
+					<div className={s.StudentCard}>
+						<p>Адрес:</p>
+						<input
+							type="text"
+							value={address}
+							onChange={(e) => setAddress(e.target.value)}
+						/>
+					</div>
+					<Line width="296px" className={s.Line} />
+					<div className={s.StudentCard}>
+						<p>Источник:</p>
+						<input
+							type="text"
+							value={linkStudent}
+							onChange={(e) => setLinkStudent(e.target.value)}
+						/>
+					</div>
+					<Line width="296px" className={s.Line} />
+					<div className={s.StudentCard}>
+						<p>Расходы по ученику:</p>
+						<input
+							type="text"
+							value={costStudent}
+							onChange={(e) => setCostStudent(e.target.value)}
+						/>
+					</div>
+					<Line width="296px" className={s.Line} />
+					<div className={s.StudentCard}>
+						<p>Комментарий:</p>
+						<textarea
+							value={commentStudent}
+							onChange={(e) => setCommentStudent(e.target.value)}
+						/>
+					</div>
+					<Line width="296px" className={s.Line} />
+				</div>
+
+				<div className={s.RecordNListen}>
+					<button className={s.Record}>
+						<p>Аудио</p>
+						<img src={microSVG} alt={microSVG} />
+					</button>
+					<button className={s.Listen}>
+						<p>Прослушать</p>
+						<img src={Listen} alt={Listen} />
+					</button>
+				</div>
+
+				<div className={s.ItemWrapper}>
+					<div className={s.ItemHeader}>
+						<div className={s.dataSlidePicker}>
+							<button className={s.btn}>
+								<span>
+									<Arrow direction={ArrowType.left} />
+								</span>
+							</button>
+							<p className={s.btnText}>Предмет &frac14;</p>
+							<button className={s.btn}>
+								<span>
+									<Arrow direction={ArrowType.right} />
+								</span>
+							</button>
+						</div>
+						<button className={s.ItemPlus}>
+							<img src={Plus} alt={Plus} />
+						</button>
+					</div>
+
+					<Line width="296px" className={s.Line} />
+
+					<div className={s.ItemMain}></div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default AddStudent
--- client/src/components/LeftMenu/index.tsx
+++ client/src/components/LeftMenu/index.tsx
@@ -1,72 +1,110 @@
 import s from './index.module.scss'
-import * as mui from '@mui/base'
-import * as React from 'react'
+import * as mui from '@mui/material'
 import Line from '../Line'
 import Search from '../../assets/search'
+import {useState} from 'react'
+import './index.css'
+import AddStudent from '../AddStudent'
 interface ILeftMenu {}
 
 const LeftMenu = ({}: ILeftMenu) => {
-	const [type, setType] = React.useState<string>('')
-	const [archive, setArchive] = React.useState<string>('')
-	const [search,setSearch] = React.useState<string>('')
+	const [type, setType] = useState<string>('')
+	const [archive, setArchive] = useState<string>('')
+	const [search, setSearch] = useState<string>('')
+	const [valueMuiSelectType, setValueMuiSelectType] = useState<string>('')
+	const [valueMuiSelectArchive, setValueMuiSelectArchive] = useState<string>('')
+	const data_muiSelectType = [
+		{
+			label: 'Все',
+			value: '40',
+		},
+		{
+			label: 'Заказчики',
+			value: '50',
+		},
+		{
+			label: 'Ученики',
+			value: '20',
+		},
+	]
 	return (
-		<div className={s.wrapper}>
-			<div className={s.HeaderLeftMenu}>
-				<div className={s.FilterNArchive}>
-					<mui.Select
-						className={s.muiSelectType}
-						renderValue={(option: mui.SelectOption<number> | null) => {
-							if (option == null || option.value === null) {
-								return (
-									<>
-										<p className={s.muiSelectTypeReturn}>Все</p>
-									</>
-								)
-							}
-							return `${option.label}`
-						}}>
-						<mui.Option value={1}>
-							<p>Все</p>
-						</mui.Option>
-						<mui.Option value={2}>
-							<p>Заказчики</p>
-						</mui.Option>
-						<mui.Option value={3}>
-							<p>Ученики</p>
-						</mui.Option>
-					</mui.Select>
-					<Line width="264px" className={s.Line} />
-					<mui.Select
-						className={s.muiSelectArchive}
-						renderValue={(option: mui.SelectOption<number> | null) => {
-							if (option == null || option.value === null) {
-								return (
-									<>
-										<p className={s.muiSelectArchiveReturn}>С архивом</p>
-									</>
-								)
-							}
-							return `${option.label}`
-						}}>
-						<mui.Option value={1}>
-							<p>С архивом</p>
-						</mui.Option>
-						<mui.Option value={2}>
-							<p>Без архива</p>
-						</mui.Option>
-					</mui.Select>
-				</div>
-				<div className={s.SearchInput}>
-					<input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Имя" />
-					<div className={s.SearchIconDiv}>
-						<Search className={s.SearchIcon}/>
-					</div>
-				</div>
-			</div>
-			<div className={s.MainLeftMenu}>
-				
-			</div>
-		</div>
+		<AddStudent/>
+		// <div className={s.wrapper}>
+		// 	<div className={s.HeaderLeftMenu}>
+		// 		<div className={s.FilterNArchive}>
+					
+		// 			<mui.Select
+		// 				className={s.muiSelectType}
+		// 				displayEmpty
+		// 				variant={'standard'}
+		// 				value={valueMuiSelectType}
+		// 				onChange={(e) => {
+		// 					setValueMuiSelectType(e.target.value)
+		// 				}}>
+		// 				{data_muiSelectType.map((item, index) => (
+		// 					<mui.MenuItem value={item.label} key={index}>
+		// 						<div
+		// 							style={{
+		// 								display: 'flex',
+		// 								flexDirection: 'row',
+		// 								whiteSpace: 'nowrap',
+		// 								fontSize: '16px',
+		// 							}}>
+		// 							<p>{`${item.label}`}</p>&nbsp;
+		// 							<p style={{fontWeight: '600'}}>{item.value}</p>
+		// 						</div>
+		// 					</mui.MenuItem>
+		// 				))}
+		// 			</mui.Select>
+
+		// 			<Line width="264px" className={s.Line} />
+
+		// 			<mui.Select
+		// 				className={s.muiSelectType}
+		// 				variant={'standard'}
+		// 				value={valueMuiSelectArchive}
+		// 				onChange={(e) => {
+		// 					setValueMuiSelectArchive(e.target.value)
+		// 				}}>
+		// 				<mui.MenuItem value={'С архивом'}>
+		// 					<div
+		// 						style={{
+		// 							display: 'flex',
+		// 							flexDirection: 'row',
+		// 							whiteSpace: 'nowrap',
+		// 							fontSize: '16px',
+		// 						}}>
+		// 						<p>С архивом</p>
+		// 					</div>
+		// 				</mui.MenuItem>
+		// 				<mui.MenuItem value={'Только архив'}>
+		// 					<div
+		// 						style={{
+		// 							display: 'flex',
+		// 							flexDirection: 'row',
+		// 							whiteSpace: 'nowrap',
+		// 							fontSize: '16px',
+		// 						}}>
+		// 						<p>Только архив</p>
+		// 					</div>
+		// 				</mui.MenuItem>
+		// 			</mui.Select>
+					
+		// 		</div>
+		// 		<div className={s.SearchInput}>
+		// 			<input
+		// 				type="text"
+		// 				value={search}
+		// 				onChange={(e) => setSearch(e.target.value)}
+		// 				placeholder="Имя"
+		// 			/>
+		// 			<div className={s.SearchIconDiv}>
+		// 				<Search className={s.SearchIcon} />
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// 	<div className={s.MainLeftMenu}></div>
+		// </div>
 	)
 }
 
