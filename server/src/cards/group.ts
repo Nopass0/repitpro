a232ac8b4392d7c1a5befc54de6552ab7b5ff952
Nopass0 <gal.bogdan2015@gmail.com>
2024-07-18 19:49:19 +0500

    fix

--- server/src/cards/group.ts
+++ server/src/cards/group.ts
@@ -11,6 +11,312 @@ function getDay(date) {
   return dayIndex === -1 ? 6 : dayIndex;
 }
 
+// export async function addGroup(data: any, socket: any) {
+//   try {
+//     const {
+//       groupName,
+//       items,
+//       students,
+//       token,
+//       files = [],
+//       filesItems = [],
+//       audiosItems = [],
+//       audiosStudents = [],
+//       historyLessons,
+//     } = data;
+
+//     const token_ = await db.token.findFirst({
+//       where: { token },
+//     });
+
+//     if (!token_) {
+//       throw new Error("Invalid token");
+//     }
+
+//     const userId = token_.userId;
+
+//     if (!userId) {
+//       throw new Error("Invalid token");
+//     }
+
+//     const conflicts = [];
+
+//     for (const item of items) {
+//       const startDate = new Date(item.startLesson);
+//       const endDate = new Date(item.endLesson);
+//       const daysToAdd = differenceInDays(endDate, startDate);
+//       const dateRange = Array.from({ length: daysToAdd + 1 }, (_, i) =>
+//         addDays(startDate, i)
+//       );
+
+//       for (const date of dateRange) {
+//         const dayOfWeek = getDay(date);
+//         const scheduleForDay = item.timeLinesArray[dayOfWeek];
+//         const dayOfMonth = date.getDate();
+
+//         const cacheKey = `${userId}-${dayOfMonth}-${
+//           date.getMonth() + 1
+//         }-${date.getFullYear()}`;
+//         let existingSchedules = cache.get(cacheKey);
+
+//         if (!existingSchedules) {
+//           existingSchedules = await db.studentSchedule.findMany({
+//             where: {
+//               day: dayOfMonth.toString(),
+//               month: (date.getMonth() + 1).toString(),
+//               year: date.getFullYear().toString(),
+//               userId,
+//             },
+//           });
+//           cache.set(cacheKey, existingSchedules, 3600000); // 1 час TTL
+//         }
+
+//         const conflictingSchedules = existingSchedules.filter((schedule) => {
+//           if (!schedule.timeLinesArray || !schedule.timeLinesArray[dayOfWeek]) {
+//             return false;
+//           }
+//           const scheduleStartTime =
+//             schedule.timeLinesArray[dayOfWeek].startTime;
+//           const scheduleEndTime = schedule.timeLinesArray[dayOfWeek].endTime;
+
+//           const newStartTime = scheduleForDay.startTime;
+//           const newEndTime = scheduleForDay.endTime;
+
+//           return (
+//             (newStartTime.hour < scheduleEndTime.hour ||
+//               (newStartTime.hour === scheduleEndTime.hour &&
+//                 newStartTime.minute <= scheduleEndTime.minute)) &&
+//             (newEndTime.hour > scheduleStartTime.hour ||
+//               (newEndTime.hour === scheduleStartTime.hour &&
+//                 newEndTime.minute >= scheduleStartTime.minute))
+//           );
+//         });
+
+//         if (conflictingSchedules.length > 0) {
+//           const daysOfWeek = [
+//             "Воскресенье",
+//             "Понедельник",
+//             "Вторник",
+//             "Среда",
+//             "Четверг",
+//             "Пятница",
+//             "Суббота",
+//           ];
+//           const dayName = daysOfWeek[dayOfWeek];
+//           const startTime = `${scheduleForDay.startTime.hour}:${scheduleForDay.startTime.minute}`;
+//           const endTime = `${scheduleForDay.endTime.hour}:${scheduleForDay.endTime.minute}`;
+
+//           let errorMessage = `В ${dayName} на данное время ${startTime}-${endTime} уже есть занятие`;
+
+//           const freeTimeSlots = [];
+//           for (const schedule of existingSchedules) {
+//             if (schedule.timeLinesArray && schedule.timeLinesArray[dayOfWeek]) {
+//               const scheduleStartTime =
+//                 schedule.timeLinesArray[dayOfWeek].startTime;
+//               const scheduleEndTime =
+//                 schedule.timeLinesArray[dayOfWeek].endTime;
+//               freeTimeSlots.push(
+//                 `${scheduleEndTime.hour}:${scheduleEndTime.minute}-${scheduleStartTime.hour}:${scheduleStartTime.minute}`
+//               );
+//             }
+//           }
+
+//           if (freeTimeSlots.length > 0) {
+//             errorMessage += `, в этот день есть свободные промежутки: ${freeTimeSlots.join(
+//               ", "
+//             )}`;
+//           }
+
+//           io.emit("addGroup", { error: errorMessage, ok: false });
+//           return;
+//         }
+//       }
+//     }
+
+//     const createdGroup = await db.group.create({
+//       data: {
+//         groupName,
+//         userId,
+//         historyLessons: historyLessons,
+//         items: {
+//           create: items.map((item) => ({
+//             itemName: item.itemName,
+//             tryLessonCheck: item.tryLessonCheck || false,
+//             tryLessonCost: item.tryLessonCost || "",
+//             todayProgramStudent: item.todayProgramStudent || "",
+//             targetLesson: item.targetLesson || "",
+//             programLesson: item.programLesson || "",
+//             typeLesson: Number(item.typeLesson) || 1,
+//             placeLesson: item.placeLesson || "",
+//             timeLesson: item.timeLesson || "",
+//             costOneLesson: "",
+
+//             valueMuiSelectArchive: item.valueMuiSelectArchive || 1,
+//             startLesson: item.startLesson ? new Date(item.startLesson) : null,
+//             endLesson: item.endLesson ? new Date(item.endLesson) : null,
+//             nowLevel: item.nowLevel || 0,
+//             lessonDuration: Number(item.lessonDuration) || null,
+//             timeLinesArray: item.timeLinesArray || {},
+//             userId,
+//           })),
+//         },
+//         students: {
+//           create: students.map((student) => ({
+//             nameStudent: student.nameStudent,
+//             contactFace: student.contactFace,
+//             phoneNumber: student.phoneNumber,
+//             email: student.email,
+//             address: student.address || "",
+//             linkStudent: student.linkStudent || "",
+//             costStudent: student.costStudent || "",
+//             commentStudent: student.commentStudent || "",
+//             prePayCost: student.prePayCost || "",
+//             prePayDate: student.prePayDate
+//               ? new Date(student.prePayDate)
+//               : null,
+//             selectedDate: null,
+//             storyLesson: student.storyLesson || "",
+//             costOneLesson: String(student.costOneLesson) || "",
+//             targetLessonStudent: student.targetLessonStudent || "",
+//             todayProgramStudent: student.todayProgramStudent || "",
+//             startLesson: student.startLesson
+//               ? new Date(student.startLesson)
+//               : null,
+//             endLesson: student.endLesson ? new Date(student.endLesson) : null,
+//             nowLevel: student.nowLevel || 0,
+//             tryLessonCost: student.tryLessonCost || "",
+//             tryLessonCheck: student.tryLessonCheck || false,
+//             userId,
+//           })),
+//         },
+//       },
+//       select: {
+//         _count: true,
+//         id: true,
+//         groupName: true,
+//         userId: true,
+//         items: true,
+//         isArchived: true,
+//         students: true,
+//       },
+//     });
+
+//     for (const item of createdGroup.items) {
+//       const startDate = new Date(item.startLesson);
+//       const endDate = new Date(item.endLesson);
+//       const daysToAdd = differenceInDays(endDate, startDate);
+//       const dateRange = Array.from({ length: daysToAdd + 1 }, (_, i) =>
+//         addDays(startDate, i)
+//       );
+
+//       for (const date of dateRange) {
+//         const dayOfWeek = getDay(date);
+//         const scheduleForDay = item.timeLinesArray[dayOfWeek];
+//         const dayOfMonth = date.getDate();
+
+//         const cond =
+//           scheduleForDay.startTime.hour === 0 &&
+//           scheduleForDay.startTime.minute === 0 &&
+//           scheduleForDay.endTime.hour === 0 &&
+//           scheduleForDay.endTime.minute === 0;
+
+//         const costOneLesson = await db.group.findUnique({
+//           where: { id: createdGroup.id },
+//           select: {
+//             students: {
+//               where: { userId },
+//               select: { costOneLesson: true },
+//             },
+//           },
+//         });
+
+//         if (!cond) {
+//           await db.studentSchedule.create({
+//             data: {
+//               day: dayOfMonth.toString(),
+//               groupId: createdGroup.id,
+//               workCount: 0,
+//               lessonsCount: 1,
+//               lessonsPrice: Number(costOneLesson.students[0].costOneLesson),
+//               workPrice: 0,
+//               month: (date.getMonth() + 1).toString(),
+//               timeLinesArray: item.timeLinesArray,
+//               isChecked: false,
+//               itemName: item.itemName,
+//               typeLesson: item.typeLesson,
+//               year: date.getFullYear().toString(),
+//               itemId: item.id,
+//               userId,
+//             },
+//           });
+//         }
+//       }
+//     }
+
+//     let filesIds = [];
+//     let filesItemsIds = [];
+
+//     if (files.length > 0) {
+//       filesIds = await upload(files, userId, "group/files", (ids) => {
+//         filesIds = ids;
+//       });
+//     }
+
+//     if (filesItems.length > 0) {
+//       filesItemsIds = await upload(
+//         filesItems,
+//         userId,
+//         "group/filesItems",
+//         (ids) => {
+//           filesItemsIds = ids;
+//         }
+//       );
+//     }
+
+//     let audiosItemsIds = [];
+//     let audiosStudentsIds = [];
+
+//     if (audiosItems.length > 0) {
+//       audiosItemsIds = await upload(
+//         audiosItems,
+//         userId,
+//         "group/audioItems",
+//         (ids) => {
+//           audiosItemsIds = ids;
+//         }
+//       );
+//     }
+
+//     if (audiosStudents.length > 0) {
+//       audiosStudentsIds = await upload(
+//         audiosStudents,
+//         userId,
+//         "group/audioStudents",
+//         (ids) => {
+//           audiosStudentsIds = ids;
+//         }
+//       );
+//     }
+
+//     await db.group.update({
+//       where: { id: createdGroup.id },
+//       data: {
+//         files: [
+//           ...filesIds,
+//           ...filesItemsIds,
+//           ...audiosItemsIds,
+//           ...audiosStudentsIds,
+//         ],
+//       },
+//     });
+
+//     socket.emit("addGroup", { ok: true });
+//   } catch (error) {
+//     console.error("Error creating group:", error);
+//     socket.emit("addGroup", { error: error.message, ok: false });
+//   }
+// }
+
 export async function addGroup(data: any, socket: any) {
   try {
     const {
@@ -40,6 +346,7 @@ export async function addGroup(data: any, socket: any) {
     }
 
     const conflicts = [];
+    const freeSlots = {};
 
     for (const item of items) {
       const startDate = new Date(item.startLesson);
@@ -127,8 +434,17 @@ export async function addGroup(data: any, socket: any) {
             )}`;
           }
 
-          io.emit("addGroup", { error: errorMessage, ok: false });
+          io.emit("addGroup", { error: errorMessage, freeSlots, ok: false });
           return;
+        } else {
+          const freeSlot = {
+            startTime: `${scheduleForDay.startTime.hour}:${scheduleForDay.startTime.minute}`,
+            endTime: `${scheduleForDay.endTime.hour}:${scheduleForDay.endTime.minute}`,
+          };
+          if (!freeSlots[dayOfWeek]) {
+            freeSlots[dayOfWeek] = [];
+          }
+          freeSlots[dayOfWeek].push(freeSlot);
         }
       }
     }
--- server/src/cards/student.ts
+++ server/src/cards/student.ts
@@ -34,10 +34,287 @@ const days = [
   "Воскресенье",
 ];
 
+// export async function addStudent(data, socket: any) {
+//   try {
+//     // console.log(data, "Schedule", data.items[0].timeLinesArray[0]?.startTime); // Safe access with ?
+
+//     const {
+//       nameStudent,
+//       phoneNumber,
+//       contactFace,
+//       email,
+//       prePayCost,
+//       prePayDate,
+//       costOneLesson,
+//       commentStudent,
+//       linkStudent,
+//       costStudent,
+//       audios,
+//       cost,
+//       historyLessons,
+//       files,
+//       items,
+//       token,
+//     } = data;
+
+//     const token_ = await db.token.findFirst({ where: { token } });
+
+//     if (!token_) {
+//       throw new Error("Invalid token");
+//     }
+
+//     const userId = token_.userId;
+
+//     if (!userId) {
+//       throw new Error("Invalid token");
+//     }
+
+//     const conflicts = [];
+
+//     for (const item of items) {
+//       const startDate = new Date(item.startLesson);
+//       const endDate = new Date(item.endLesson);
+//       const daysToAdd = differenceInDays(endDate, startDate);
+//       const dateRange = Array.from({ length: daysToAdd + 1 }, (_, i) =>
+//         addDays(startDate, i)
+//       );
+
+//       for (const date of dateRange) {
+//         const dayOfWeek = getDay(date);
+//         const scheduleForDay = item.timeLinesArray[dayOfWeek];
+
+//         if (!scheduleForDay) {
+//           console.warn(
+//             `No schedule defined for day of week: ${dayOfWeek} on date: ${date}`
+//           );
+//           continue; // Skip this iteration if no schedule is defined for the day
+//         }
+
+//         const dayOfMonth = date.getDate();
+//         const cacheKey = `${userId}-${dayOfMonth}-${
+//           date.getMonth() + 1
+//         }-${date.getFullYear()}`;
+//         let existingSchedules = cache.get(cacheKey);
+
+//         if (!existingSchedules) {
+//           existingSchedules = await db.studentSchedule.findMany({
+//             where: {
+//               day: dayOfMonth.toString(),
+//               month: (date.getMonth() + 1).toString(),
+//               year: date.getFullYear().toString(),
+//               userId,
+//             },
+//           });
+//           cache.set(cacheKey, existingSchedules, 3600000); // 1 hour TTL
+//         }
+
+//         const conflictingSchedules = existingSchedules.filter((schedule) => {
+//           const scheduleStartTime =
+//             schedule.timeLinesArray[dayOfWeek]?.startTime;
+//           const scheduleEndTime = schedule.timeLinesArray[dayOfWeek]?.endTime;
+
+//           if (!scheduleStartTime || !scheduleEndTime) {
+//             console.warn(
+//               `Incomplete schedule found for day of week: ${dayOfWeek} in existing schedules`
+//             );
+//             return false;
+//           }
+
+//           const newStartTime = scheduleForDay.startTime;
+//           const newEndTime = scheduleForDay.endTime;
+
+//           return (
+//             (newStartTime.hour < scheduleEndTime.hour ||
+//               (newStartTime.hour === scheduleEndTime.hour &&
+//                 newStartTime.minute <= scheduleEndTime.minute)) &&
+//             (newEndTime.hour > scheduleStartTime.hour ||
+//               (newEndTime.hour === scheduleStartTime.hour &&
+//                 newEndTime.minute >= scheduleStartTime.minute))
+//           );
+//         });
+
+//         if (conflictingSchedules.length > 0) {
+//           const daysOfWeek = [
+//             "Понедельник",
+//             "Вторник",
+//             "Среда",
+//             "Четверг",
+//             "Пятница",
+//             "Суббота",
+//             "Воскресенье",
+//           ];
+//           const dayName = daysOfWeek[dayOfWeek];
+//           const startTime = `${scheduleForDay.startTime.hour}:${scheduleForDay.startTime.minute}`;
+//           const endTime = `${scheduleForDay.endTime.hour}:${scheduleForDay.endTime.minute}`;
+
+//           let conflict = {
+//             day: dayName,
+//             timeLines: conflictingSchedules.map((schedule) => {
+//               const scheduleStartTime =
+//                 schedule.timeLinesArray[dayOfWeek]?.startTime;
+//               const scheduleEndTime =
+//                 schedule.timeLinesArray[dayOfWeek]?.endTime;
+//               return {
+//                 time: `${scheduleStartTime.hour}:${scheduleStartTime.minute}-${scheduleEndTime.hour}:${scheduleEndTime.minute}`,
+//               };
+//             }),
+//           };
+
+//           conflicts.push(conflict);
+//         }
+//       }
+//     }
+
+//     if (conflicts.length > 0) {
+//       io.emit("addStudent", { error: conflicts, ok: false });
+//       return;
+//     }
+
+//     const createdGroup = await db.group.create({
+//       data: {
+//         groupName: "",
+//         userId,
+//         historyLessons: JSON.parse(JSON.stringify(historyLessons)),
+//         items: {
+//           create: items.map((item) => ({
+//             itemName: item.itemName,
+//             tryLessonCheck: item.tryLessonCheck || false,
+//             tryLessonCost: item.tryLessonCost || "",
+//             todayProgramStudent: item.todayProgramStudent || "",
+//             targetLesson: item.targetLesson || "",
+//             programLesson: item.programLesson || "",
+//             typeLesson: Number(item.typeLesson) || 1,
+//             placeLesson: item.placeLesson || "",
+//             timeLesson: item.timeLesson || "",
+//             valueMuiSelectArchive: item.valueMuiSelectArchive || 1,
+//             startLesson: item.startLesson ? new Date(item.startLesson) : null,
+//             endLesson: item.endLesson ? new Date(item.endLesson) : null,
+//             nowLevel: item.nowLevel || 0,
+//             costOneLesson: item.costOneLesson || "",
+//             lessonDuration: item.lessonDuration || null,
+//             timeLinesArray: item.timeLinesArray || {},
+//             userId,
+//           })),
+//         },
+//         students: {
+//           create: [
+//             {
+//               nameStudent,
+//               contactFace,
+//               phoneNumber,
+//               email,
+//               address: "",
+//               linkStudent: linkStudent || "",
+//               costStudent: costStudent || "",
+//               commentStudent,
+//               prePayCost,
+//               prePayDate: prePayDate ? new Date(prePayDate) : null,
+//               selectedDate: null,
+//               storyLesson: "",
+//               costOneLesson,
+//               targetLessonStudent: "",
+//               todayProgramStudent: "",
+//               userId,
+//             },
+//           ],
+//         },
+//       },
+//       select: {
+//         id: true,
+//         _count: true,
+//         isArchived: true,
+//         groupName: true,
+//         students: true,
+//         userId: true,
+//         items: true,
+//       },
+//     });
+
+//     for (const item of createdGroup.items) {
+//       const startDate = new Date(item.startLesson);
+//       const endDate = new Date(item.endLesson);
+//       const daysToAdd = differenceInDays(endDate, startDate);
+//       const dateRange = Array.from({ length: daysToAdd + 1 }, (_, i) =>
+//         addDays(startDate, i)
+//       );
+
+//       for (const date of dateRange) {
+//         const dayOfWeek = getDay(date);
+//         const scheduleForDay = item.timeLinesArray[dayOfWeek];
+
+//         if (!scheduleForDay) {
+//           console.warn(
+//             `No schedule defined for day of week: ${dayOfWeek} on date: ${date}`
+//           );
+//           continue;
+//         }
+
+//         const cond =
+//           scheduleForDay.startTime.hour === 0 &&
+//           scheduleForDay.startTime.minute === 0 &&
+//           scheduleForDay.endTime.hour === 0 &&
+//           scheduleForDay.endTime.minute === 0;
+
+//         if (!cond) {
+//           await db.studentSchedule.create({
+//             data: {
+//               day: date.getDate().toString(),
+//               groupId: createdGroup.id,
+//               workCount: 0,
+//               lessonsCount: 1,
+//               lessonsPrice: Number(item.costOneLesson),
+//               workPrice: 0,
+//               month: (date.getMonth() + 1).toString(),
+//               timeLinesArray: item.timeLinesArray,
+//               isChecked: false,
+//               itemName: item.itemName,
+//               studentName: nameStudent,
+//               typeLesson: item.typeLesson,
+//               year: date.getFullYear().toString(),
+//               itemId: item.id,
+//               userId,
+//             },
+//           });
+//         }
+//       }
+//     }
+
+//     let filePaths = [];
+
+//     if (files.length > 0) {
+//       filePaths = await upload(files, userId, "", (ids) => {
+//         filePaths = ids;
+//       });
+//     }
+
+//     let audiosIds = [];
+
+//     if (audios.length > 0) {
+//       audiosIds = await upload(audios, userId, "student/audio", (ids) => {
+//         audiosIds = ids;
+//       });
+//     }
+
+//     filePaths = [...filePaths, ...audiosIds];
+
+//     await db.student.update({
+//       where: {
+//         id: createdGroup.students[0].id,
+//       },
+//       data: {
+//         files: filePaths,
+//       },
+//     });
+
+//     socket.emit("addStudent", { ok: true });
+//   } catch (error) {
+//     console.error("Error creating group:", error);
+//     socket.emit("addStudent", { error: error.message, ok: false });
+//   }
+// }
+
 export async function addStudent(data, socket: any) {
   try {
-    // console.log(data, "Schedule", data.items[0].timeLinesArray[0]?.startTime); // Safe access with ?
-
     const {
       nameStudent,
       phoneNumber,
@@ -70,6 +347,7 @@ export async function addStudent(data, socket: any) {
     }
 
     const conflicts = [];
+    const freeSlots = {};
 
     for (const item of items) {
       const startDate = new Date(item.startLesson);
@@ -161,12 +439,21 @@ export async function addStudent(data, socket: any) {
           };
 
           conflicts.push(conflict);
+        } else {
+          const freeSlot = {
+            startTime: `${scheduleForDay.startTime.hour}:${scheduleForDay.startTime.minute}`,
+            endTime: `${scheduleForDay.endTime.hour}:${scheduleForDay.endTime.minute}`,
+          };
+          if (!freeSlots[dayOfWeek]) {
+            freeSlots[dayOfWeek] = [];
+          }
+          freeSlots[dayOfWeek].push(freeSlot);
         }
       }
     }
 
     if (conflicts.length > 0) {
-      io.emit("addStudent", { error: conflicts, ok: false });
+      io.emit("addStudent", { error: conflicts, freeSlots, ok: false });
       return;
     }
 
